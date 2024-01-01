d28638953d5ae9fddf01dc3879246437b1d874f7
Serhan Khan <khanboy0689@gmail.com>
2024-01-01 16:04:30 +0100

    first commit

---
+++ MarvelSquad/MarvelSquadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelSquadApp.swift
+//  MarvelSquad
+//
+//  Created by Serhan Khan on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelSquadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
