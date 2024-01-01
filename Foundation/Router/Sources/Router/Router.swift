1b856a52f4b0a3cbb02a40842b6fe145f5198c12
Serhan Khan <khanboy0689@gmail.com>
2024-01-01 17:30:27 +0100

    Router swift package created and added to git

---
+++ Foundation/Router/Sources/Router/Router.swift
@@ -0,0 +1,36 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public class AnyIdentifiable: Identifiable {
+    
+    public let destination: any Identifiable
+    
+    public init(destination: any Identifiable) {
+        self.destination = destination
+    }
+}
+
+public  final class Router: ObservableObject {
+    @Published public var navPath = NavigationPath()
+    @Published public var presentedSheet: AnyIdentifiable?
+    
+    public init() {}
+    
+    public func presentSheet(destination: any Identifiable) {
+        presentedSheet = AnyIdentifiable(destination: destination)
+    }
+    
+    public func navigate(to destination: any Hashable) {
+        navPath.append(destination)
+    }
+    
+    public func navigateBack() {
+        navPath.removeLast()
+    }
+    
+    public func navigateToRoot() {
+        navPath.removeLast(navPath.count)
+    }
+}
