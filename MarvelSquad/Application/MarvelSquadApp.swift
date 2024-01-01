595a9ca322ae0255a24dcaa5e6aac19f3404d7a6
Serhan Khan <khanboy0689@gmail.com>
2024-01-01 18:12:43 +0100

    MainTabView SwiftUI added

--- MarvelSquad/Application/MarvelSquadApp.swift
+++ MarvelSquad/Application/MarvelSquadApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Router
 
 @main
 struct MarvelSquadApp: App {
---
+++ MarvelSquad/Presentation/TabView/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  MarvelSquad
+//
+//  Created by Serhan Khan on 01/01/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selection = 0
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
