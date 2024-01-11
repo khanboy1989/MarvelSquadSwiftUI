4c3486b0aa4dd190b8cb3f9fee5199b6e2a40d16
Serhan Khan <khanboy0689@gmail.com>
2024-01-12 00:45:14 +0100

    added macros folder and files

---
+++ Foundation/Helpers/Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import CompilerPluginSupport
+
+let package = Package(
+    name: "Helpers",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Helpers",
+            targets: ["Helpers"]),
+        .library(name: "Macros", targets: ["Macros"])
+    ],
+    dependencies: [
+        // Depend on the Swift 5.9 release of SwiftSyntax
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.1.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "Macros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+
+        .target(
+            name: "Helpers"),
+        
+        .testTarget(
+            name: "HelpersTests",
+            dependencies: ["Helpers"]),
+    ]
+)
---
+++ Foundation/Helpers/Sources/Macros/DefaultInitMacro.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Serhan Khan on 12/01/2024.
+//
+
+import Foundation
+
+public struct DefaultInitMacro {
+    
+}
