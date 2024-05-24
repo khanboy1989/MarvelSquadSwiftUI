9f987a73a0f4deb351c6398b5415bb2664e46a65
Serhan Khan <serhankhan0@gmail.com>
2024-05-24 13:02:27 +0200

    impl: System Design Package for Assets and Localisable

--- Foundation/SystemDesign/Package.swift
+++ Foundation/SystemDesign/Package.swift
@@ -19,7 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SystemDesign"),
+            name: "SystemDesign",
+            resources: [
+                .process("Supporting/Images.xcassets"),
+                .process("Supporting/Colors.xcassets"),
+                .process("Supporting/en.lproj/Localizable.strings"),
+            ]),
         .testTarget(
             name: "SystemDesignTests",
             dependencies: ["SystemDesign"]),
