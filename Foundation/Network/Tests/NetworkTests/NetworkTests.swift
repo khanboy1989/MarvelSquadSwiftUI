afcdb602873eb30efed08fafa727c809f4a07d2e
Serhan Khan <serhankhan0@gmail.com>
2024-06-19 16:29:12 +0200

    comment: testApiClientRequestMethod purpose

--- Foundation/Network/Tests/NetworkTests/NetworkTests.swift
+++ Foundation/Network/Tests/NetworkTests/NetworkTests.swift
@@ -7,6 +7,9 @@ final class NetworkTests: XCTestCase {
     override func setUp() {
         apiClientService = createAPIClientServiceMock()
     }
+    
+    /// Purpose: Tests whether apiClientService class creates the baseUrl and headers correctly
+    /// Obviously it will return error from request method since all of the requirements for the request is not provided
     func testApiClientRequestMethod() async {
         let result = await apiClientService.request(APIEndpoint(path: "/v1/public/characters", httpMethod: .get))
         XCTAssertNotNil(result)
