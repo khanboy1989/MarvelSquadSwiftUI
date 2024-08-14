84e8826d8b82b380a49a93f5a70a0cd210f5de37
Serhan Khan <khanboy0689@gmail.com>
2024-08-14 22:40:28 +0300

    fix: apiClientService nil after test completion

--- Foundation/Network/Tests/NetworkTests/NetworkTests.swift
+++ Foundation/Network/Tests/NetworkTests/NetworkTests.swift
@@ -5,8 +5,15 @@ import XCTest
 final class NetworkTests: XCTestCase {
     var apiClientService: IAPIClientService!
     override func setUp() {
+        super.setUp()
         apiClientService = createAPIClientServiceMock()
     }
+    
+    override func tearDown() {
+        super.tearDown()
+        apiClientService = nil 
+    }
+    
     /// Purpose: Tests whether apiClientService class creates the baseUrl and headers correctly
     /// Obviously it will return error from request method since all of the requirements for the request is not provided
     func testApiClientRequestMethod() async {
